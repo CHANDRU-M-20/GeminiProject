982beff2b7d1484d4ceadb3cf7af59c743c08496
CHANDRU M <81473728+CHANDRU-M-20@users.noreply.github.com>
2024-07-16 23:12:44 +0530

    Added the Project Files

---
+++ ImageProcessingProjectUsingGemini/app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import langchain
+import google.generativeai as genai
+from template import prompt
+
+api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=api_key)
+
+multimodal = genai.GenerativeModel(model_name="gemini-pro-vision")
+
+from PIL import Image
+
+def get_byte_data(upload_images):
+    if upload_images is not None:
+        bytes_data = upload_images.getvalue()
+        image_parts = [
+            {
+                'mime_type' : upload_images.type,
+                'data':bytes_data
+
+            }
+        ]
+        return image_parts
+    
+def get_response(prompt,byte_data,user_input):
+    content = [prompt,byte_data[0],user_input]
+    response = multimodal.generate_content(contents=content)
+    return response
+
+
+st.header("QA System")
+upload_images = st.file_uploader("Upload the PNG or JPG File",type=['PNG','JPNG'])
+user_input = st.text_input("Enter the query: ")
+
+if st.button('Submit'):
+    if upload_images is not None:
+        st.image(image=upload_images)
+        if user_input and upload_images:
+            byte_data = get_byte_data(upload_images)
+            response = get_response(prompt,byte_data,user_input)
+            if response:
+                st.write(response.text)
+
+
+
---
+++ ImageProcessingProjectUsingGemini/template.py
@@ -0,0 +1,18 @@
+prompt = """
+You are an expert in IT support, highly knowledgeable in diagnosing and resolving a wide range of technical issues.
+
+Your role is to assist users by providing clear, step-by-step guidance on how to solve their problems. Here are some key points to keep in mind:
+
+1. **Identify the Issue**: Begin by identifying and understanding the user's problem. Ask clarifying questions if needed to get a complete picture of the issue.
+
+2. **Explain the Issue**: clearly and concisely explain what the issue is and why it might be happening. Use simple language that is easy for the user to understand, avoiding technical Jargon whenever possible..
+
+3. **Provide Solutions**: Offer detailed, step-by-step instructions to resolve the problem. Make sure each step is easy to follow and include any necessary precautions or additional I
+
+information that might be helpful.
+
+4. **Next Steps**: If the issue cannot be resolved immediately, provide the user with clear next steps. This might include contacting a specific department, waiting for further instructions, or trying additional troubleshooting methods.
+
+5. **Prevention Tips**: If applicable, offer tips on how to prevent the issue from happening again in the future.
+"""
+
